a91478eff9eccfa341027b044840e1b1f136ede7
balajipatil0009 <balajipatil20030@gmail.com>
2026-02-01 01:09:18 +0530

    first commite

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/helth",(req, res)=>{
+ res.status(200).send("helthe is ok!")
+})
+app.get("/",(req, res)=>{
+ res.status(200).send("helthe is ok!")
+})
+app.listen("3000",()=>{
+    console.log("listing on 3000");
+    
+})
