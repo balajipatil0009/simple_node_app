376ed7cf2ea20c2395cc6a2285d8bf2346eac5bb
balajipatil0009 <balajipatil20030@gmail.com>
2026-02-02 13:35:40 +0530

    testing webhook

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ app.get("/helth",(req, res)=>{
  res.status(200).send("helthe is ok!")
 })
 app.get("/",(req, res)=>{
- res.status(200).send("helthe is ok!")
+ res.status(200).send("helthe is ok!testing webhook")
 })
 app.listen("3000",()=>{
     console.log("listing on 3000");
