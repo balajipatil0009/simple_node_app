1de3f9cc553abd2f9742ed80f64a5859cb3a7137
balajipatil0009 <balajipatil20030@gmail.com>
2026-02-03 11:13:45 +0530

    test 04

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ app.get("/helth",(req, res)=>{
  res.status(200).send("helthe is ok!")
 })
 app.get("/",(req, res)=>{
- res.status(200).send("helthe is ok!testing web 3")
+ res.status(200).send("helthe is ok!testing web 03")
 })
 app.listen("3000",()=>{
     console.log("listing on 3000");
